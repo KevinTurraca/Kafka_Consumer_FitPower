3b4d5f865180a6638cf798864270dfd7cc7cdc1a
KevinTurraca <kevin.itec1@gmail.com>
2025-07-23 21:04:21 -0300

    arregaldo el archivo de configuracion kafka

--- src/main/java/com/gym/fit_power/config/KafkaConsumer.java
+++ src/main/java/com/gym/fit_power/config/KafkaConsumer.java
@@ -1,6 +1,6 @@
 package com.gym.fit_power.config;
 
-import com.gym.fit_power.notification.event.Event;
+import com.gym.fit_power.model.KafkaNotification;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -20,18 +20,18 @@ import java.util.HashMap;
 public class KafkaConsumer {
 
     @Bean
-    public ConsumerFactory<String, Event<?>> consumerFactory() {
+    public ConsumerFactory<String, KafkaNotification<?>> consumerFactory() {
         Map<String, String> props = new HashMap<>();
         String bootstrapAddress = "localhost:9092";
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(JsonSerializer.TYPE_MAPPINGS, "com.gym.fit_power:com.gym.fit_power.notification.event.Event");
-        final JsonDeserializer<Event<?>> deserializer = new JsonDeserializer<>();
+        props.put(JsonSerializer.TYPE_MAPPINGS, "com.gym.fit_power:com.gym.fit_power.model.KafkaNotification");
+        final JsonDeserializer<KafkaNotification<?>> deserializer = new JsonDeserializer<>();
         return new DefaultKafkaConsumerFactory(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Event<?>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Event<?>> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, KafkaNotification<?>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, KafkaNotification<?>> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
